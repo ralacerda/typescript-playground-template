cc8a76636275c7ed65f641267d88f6bbf24663ba
Renato Lacerda <renato.ac.lacerda@gmail.com>
2025-08-09 18:13:06 -0300

    chore: showing example with loading another module

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import { banana } from "./anotherFile.ts";
+
 function greet(name: string): string {
   return `Hello, ${name}!`;
 }
 
-const result: string = greet("TypeScript");
+const result: string = greet(`TypeScript, have a ${banana}!`);
 console.log(result);
