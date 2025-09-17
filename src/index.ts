b3f920615a525ea0639a9109fa20b88931892ba3
Renato Lacerda <renato.ac.lacerda@gmail.com>
2025-09-17 19:38:36 -0300

    chore: include json reading in example

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { banana } from "./another-file.ts";
+import database from "./database.json" with { type: "json" };
 
 function greet(name: string): string {
-  return `Hello, ${name}!`;
+  return `Hello, ${name}! Have a ${banana}!`;
 }
 
-const result: string = greet(`TypeScript, have a ${banana}!`);
+// eslint-disable-next-line @typescript-eslint/no-magic-numbers
+const user = database.users[0]?.name ?? "Guest";
+
+const result: string = greet(user);
 console.log(result);
