9bf49e7f3d7ccd294b3b01d6a469da81c086c675
Renato Lacerda <renato.ac.lacerda@gmail.com>
2025-11-03 14:32:37 -0300

    fix: include subfolder for typescript files checking

--- eslint.config.ts
+++ eslint.config.ts
@@ -5,7 +5,7 @@ export default tseslint.config([
   eslintPluginUnicorn.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    files: ["src/*.ts"],
+    files: ["src/**/*.ts"],
     plugins: {
       "@typescript-eslint": tseslint.plugin,
     },
